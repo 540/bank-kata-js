041f6e469eab0f07b2ad9aaeee72762a400d89cd
Gorka <gorka@540deg.com>
2018-05-16 17:57:45 +0200

    step4 repository fake

--- test/account.test.js
+++ test/account.test.js
@@ -3,12 +3,20 @@ import Transaction from '../src/transaction'
 
 let transactionRepository, account, printer
 
+const fakeRepository = () => {
+  let transactions = []
+
+  return {
+    save: transaction => transactions.push(transaction),
+    findAll: () => transactions,
+    _setAll: newTransactions => (transactions = newTransactions),
+    _getAll: () => transactions
+  }
+}
+
 describe('Account', () => {
   beforeEach(() => {
-    transactionRepository = {
-      save: jest.fn(),
-      findAll: jest.fn()
-    }
+    transactionRepository = fakeRepository()
     printer = {
       printHeader: jest.fn(),
       printStatement: jest.fn()
@@ -20,7 +28,7 @@ describe('Account', () => {
     it('saves a deposit', () => {
       account.deposit(1000)
 
-      expect(transactionRepository.save).toHaveBeenCalledWith(
+      expect(transactionRepository._getAll()).toContainEqual(
         new Transaction(1000)
       )
     })
@@ -28,7 +36,7 @@ describe('Account', () => {
     it('saves another deposit', () => {
       account.deposit(500)
 
-      expect(transactionRepository.save).toHaveBeenCalledWith(
+      expect(transactionRepository._getAll()).toContainEqual(
         new Transaction(500)
       )
     })
@@ -38,45 +46,43 @@ describe('Account', () => {
     it('saves a withdraw', () => {
       account.withdraw(1000)
 
-      expect(transactionRepository.save).toHaveBeenCalledWith(new Transaction(-1000))
+      expect(transactionRepository._getAll()).toContainEqual(
+        new Transaction(-1000)
+      )
     })
 
     it('saves another withdraw', () => {
       account.withdraw(500)
 
-      expect(transactionRepository.save).toHaveBeenCalledWith(new Transaction(-500))
+      expect(transactionRepository._getAll()).toContainEqual(
+        new Transaction(-500)
+      )
     })
   })
 
   describe('print statement', () => {
     it('prints statements', () => {
-      transactionRepository.findAll.mockReturnValue([
-        new Transaction(1000), new Transaction(200), new Transaction(-400)
+      transactionRepository._setAll([
+        new Transaction(1000),
+        new Transaction(200),
+        new Transaction(-400)
       ])
 
       account.printStatement()
 
       expect(printer.printHeader).toHaveBeenCalled()
-      expect(printer.printStatement).toHaveBeenCalledWith(
-        new Transaction(1000)
-      )
-      expect(printer.printStatement).toHaveBeenCalledWith(
-        new Transaction(200)
-      )
-      expect(printer.printStatement).toHaveBeenCalledWith(
-        new Transaction(-400)
-      )
+      expect(printer.printStatement).toHaveBeenCalledWith(new Transaction(1000))
+      expect(printer.printStatement).toHaveBeenCalledWith(new Transaction(200))
+      expect(printer.printStatement).toHaveBeenCalledWith(new Transaction(-400))
     })
 
     it('prints another statements ', () => {
-      transactionRepository.findAll.mockReturnValue([new Transaction(9800)])
+      transactionRepository._setAll([new Transaction(9800)])
 
       account.printStatement()
 
       expect(printer.printHeader).toHaveBeenCalled()
-      expect(printer.printStatement).toHaveBeenCalledWith(
-        new Transaction(9800)
-      )
+      expect(printer.printStatement).toHaveBeenCalledWith(new Transaction(9800))
     })
   })
 })
